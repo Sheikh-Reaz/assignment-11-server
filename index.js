130cc483fa312698271d67ee576d15b504d1ad65
Sheikh-Reaz <skreazuddin87@gmail.com>
2025-12-12 08:11:16 +0600

    all product get api

--- index.js
+++ index.js
@@ -142,6 +142,9 @@ app.post("/jwt", async (req, res) => {
     app.post("/logout", (req, res) => {
       res.clearCookie("token").send({ success: true });
     });
+    app.post("/logout", (req, res) => {
+      res.clearCookie("token").send({ success: true });
+    });
 
        /* ================================
        PROTECTED APIS
